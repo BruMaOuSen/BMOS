46109a108a9ec2396b593e62a8def1a418a80ea2
BVoinier <bruno.voinier@gmail.com>
2015-05-16 23:56:21 +0200

    définition des zones de Lyon et des prix aléatoirement pour ces zones

---
+++ zones.sql
@@ -0,0 +1,52 @@
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('La Duchère', 3, 200);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Saint Rambert', 4, 250);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Vaise', 2, 100);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Champvert - Gorge du Loup', 6, 600);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Champvert - Point du Jour - Jeunet', 3, 200);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Ménival - Battières - La Plaine', 3, 200);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Saint Just - Saint Irénée - Fourvière', 9, 950);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Vieux Lyon', 4, 250);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Pentes de la Croix-Rousse', 4, 250);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Croix-Rousse', 6, 600);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Tete d''Or', 4, 250);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Brotteaux - Bellecombe', 6, 600);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Mutualité - Préfecture', 4, 250);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Guillotière',2, 100);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Voltaire - Part Dieu', 3, 200);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Jean Macé',2, 100);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('La Mouche', 4, 250);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Gerland', 3, 200);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Grand Trou - Moulin à vent', 3, 200);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Etats-Unis',6, 600);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Monplaisir',2, 100);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Sans souci - Dauphiné', 3, 200);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Monchat',2, 100);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Bachut - Transvaal', 3, 200);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('La Plaine Santy',6, 600);
+INSERT INTO zone (nom_zone, prix_h_zone, prix_m_zone)
+VALUES ('Laennec Mermoz', 3, 200);
