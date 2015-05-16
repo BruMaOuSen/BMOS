3db69ea27bfe72c649a38f060e882eb57ead62d2
BVoinier <bruno.voinier@gmail.com>
2015-05-17 01:14:40 +0200

    ajout d'une connexion listant les zones et les tarifs pour la mairie

---
+++ mairies.php
@@ -0,0 +1,26 @@
+<?php
+	// Connexion, sélection de la base de données
+	$dbconn = pg_connect("host=localhost dbname=parkingProject user=admin password=admin")
+    	or die('Connexion impossible : ' . pg_last_error());
+
+	// Exécution de la requête SQL
+	$query = 'SELECT nom_zone, prix_h_zone, prix_m_zone FROM zone';
+	$result = pg_query($query) or die('Echec de la requête : ' . pg_last_error());
+
+	// Affichage des résultats en HTML
+	echo "<table>\n";
+	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    	echo "\t<tr>\n";
+    	foreach ($line as $col_value) {
+    	    echo "\t\t<td>$col_value</td>\n";
+    	}
+    	echo "\t</tr>\n";
+	}
+	echo "</table>\n";
+
+	// LibÃ¨re le résultat		
+	pg_free_result($result);
+
+	// Ferme la connexion
+	pg_close($dbconn);
+?>
