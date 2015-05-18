b597bdaa76fe80ac22c1635759f616c8002704c3
BVoinier <bruno.voinier@gmail.com>
2015-05-18 17:19:09 +0200

    Requete SQL pour créer un compte Mairie et un compte Admin dans la base utilisateur (on aura tous les mêmes comptes avec les mêmes mot de passe comme ça :)

---
+++ comptesMairieEtAdmin.sql
@@ -0,0 +1,4 @@
+INSERT INTO utilisateur (pseudo, mot_de_passe, type_user)
+VALUES ('admininistrateur', 'upark', 'administrateur');
+INSERT INTO utilisateur (pseudo, mot_de_passe, type_user)
+VALUES ('mairie', 'lyon', 'mairie');
