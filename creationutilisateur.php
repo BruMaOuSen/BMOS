315a685a556f3658527d803da4c286508f01cbf5
BVoinier <bruno.voinier@gmail.com>
2015-05-18 17:15:38 +0200

    debut de connexion pour les mairies

---
+++ creationutilisateur.php
@@ -0,0 +1,10 @@
+<?php
+	// Connexion, sélection de la base de données
+	$dbconn = pg_connect("host=localhost dbname=parkingProject user=admin password=admin")
+    	or die('Connexion impossible : ' . pg_last_error());
+    
+    // Exécution de la requête SQL
+	$query = "INSERT INTO utilisateur (pseudo, mot_de_passe, type_user) VALUES ('$_POST[login]', '$_POST[motdepasse]', 'client')";
+	$result = pg_query($query) or die('Echec de la requête : ' . pg_last_error());
+?>	
+<p>creation de l'utilisateur</p>
