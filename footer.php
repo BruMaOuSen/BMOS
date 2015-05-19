c9093e86a242fa167f5f642848f30ee5364f6c1d
BVoinier <bruno.voinier@gmail.com>
2015-05-19 17:13:55 +0200

    Creation du footer en php pour réutilisation sur toutes les pages.

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+    <div class="container">
+    	<div class="row">
+    		<div  class="col-md-12">
+				Propulsé par HTML, CSS, Bootstrap et quelques idées de notre part... Aucun droit réservé ;-).
+    		</div>
+    	</div>
+    </div>
+</div>
