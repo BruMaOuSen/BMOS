e0ca14f238084b3350b2aa465e4a1e84fd0c5b52
BVoinier <bruno.voinier@gmail.com>
2015-05-19 17:15:07 +0200

    Mise à jour de la page d'index

--- index.php
+++ index.php
@@ -41,14 +41,6 @@
 				</div>
     		</div>
     	</div>
-    	<div id="footer">
-    		<div class="container">
-    			<div class="row">
-    				<div  class="col-md-12">
-						Propulsé par HTML, CSS, Bootstrap et quelques idées de notre part... Aucun droit réservé ;-).
-    				</div>
-    			</div>
-    		</div>
-    	</div>
+		<?php include ('footer.php'); ?>
     </body>
 </html>
