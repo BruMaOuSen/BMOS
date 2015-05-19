7da07ba9c7adb463330863e264a4983ab0f7c3d6
BVoinier <bruno.voinier@gmail.com>
2015-05-19 17:11:08 +0200

    creation du header en php pour l'utiliser sur toutes les pages

---
+++ header.php
@@ -0,0 +1,15 @@
+<div id="header">
+    <div class="container">
+    	<div class="row">
+    		<div  class="col-md-4">
+    			<a href="index.html"><h1>UPARK</h1>
+    			<p>Les parkings de Lyon</p></a>
+    		</div>
+    		<div  class="col-md-offset-3 col-md-4">
+    			<input id="pseudo" type="text" placeholder="pseudo">
+    			<input id="password" type="password" placeholder="password">    					
+    			<button>Envoyer</button>
+    		</div>
+    	</div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -7,21 +7,7 @@
         <title>UPARK | Home</title>
     </head>
     <body>
-    	<div id="header">
-    		<div class="container">
-    			<div class="row">
-    				<div  class="col-md-4">
-    					<a href="index.html"><h1>UPARK</h1>
-    					<p>Les parkings de Lyon</p></a>
-    				</div>
-    				<div  class="col-md-offset-3 col-md-4">
-    					<input id="pseudo" type="text" placeholder="pseudo">
-    					<input id="password" type="password" placeholder="password">    					
-    					<button>Envoyer</button>
-    				</div>
-    			</div>
-    		</div>
-    	</div>
+		<?php include ('header.php');?>
     	<div id="inscription">
     		<div class="container">
     			<div class="row">
