ce5b01c80bf1b2f12cc0bc3e0c97b4a8d9b676dc
BVoinier <bruno.voinier@gmail.com>
2015-05-26 15:30:46 +0200

    Modification de la connexion pour les utilisateurs.

--- creationutilisateur.php
+++ creationutilisateur.php
@@ -1,13 +1,24 @@
 <?php session_start();?>
 <?php
 	// Connexion, sélection de la base de données
-	$dbconn = pg_connect("host=localhost dbname=parkingProject user=admin password=admin")
-    	or die('Connexion impossible : ' . pg_last_error());
+	try
+	{
+		$bdd = new PDO('pgsql:host=localhost;dbname=parkingProject', 'admin', 'admin');
+	}
+	catch (Exception $e)
+	{
+        die('Erreur : ' . $e->getMessage());
+	}
+	$reponse = $bdd->query("INSERT INTO utilisateur (pseudo, mot_de_passe, type_user) 
+							VALUES ('$_POST[login]', '$_POST[motdepasse]', 'client')");
+	
+	//$dbconn = pg_connect("host=localhost dbname=parkingProject user=admin password=admin")
+    //	or die('Connexion impossible : ' . pg_last_error());
     
     // Exécution de la requête SQL
-	$query = "INSERT INTO utilisateur (pseudo, mot_de_passe, type_user) 
-			VALUES ('$_POST[login]', '$_POST[motdepasse]', 'client')";
-	$result = pg_query($query) or die('Echec de la requête : ' . pg_last_error());
+	//$query = "INSERT INTO utilisateur (pseudo, mot_de_passe, type_user) 
+	//		VALUES ('$_POST[login]', '$_POST[motdepasse]', 'client')";
+	//$result = pg_query($query) or die('Echec de la requête : ' . pg_last_error());
 	
 	$_SESSION['membreid']= $_POST['login'];
 	$_SESSION['authentification'] = TRUE;
