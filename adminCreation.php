7b74c6d6d6f47b37013e962cfe13b04fef0fcaac
oumaimata <oumaima.ta@gmail.com>
2015-05-29 17:49:40 +0200

    affichage des parking pret

--- adminCreation.php
+++ adminCreation.php
@@ -38,7 +38,7 @@
 
   			<div class="alert alert-info alert-dismissable col-md-offset-2 col-md-8" style="display: none">
     			<button type="button" class="close">×</button>
-				<form method="post" action="adminPark.php">
+				<form method="post" action="adminCreation.php">
 					<h3 class="panel-title">Choisir une zone</h3>
   					<select name="zone"class="selectpicker">
   					<?php
@@ -61,6 +61,61 @@
     			<button type="submit" class="btn btn-info" id="afficher">
     				<span class="glyphicon glyphicon-pencil"></span>&nbsp;choisir une zone
     			</button>
+  			</div>
+
+		</div>
+
+<?php 
+			$nomzone = $_POST['zone'];
+//			$reponse2 = $bdd->query("SELECT nom_park FROM parking WHERE zone_park = '$nomzone'");
+//			$donnees2 = $reponse2->fetch();
+?>
+
+<section style="margin-top: 15px;" class="col-md-offset-2 col-md-8 table-responsive">
+			<table class="table table-bordered table-striped">
+  				<caption>
+  					<h4 style="text-align: center">Les parkings de cette zone</h4>
+  				</caption>
+  				<thead>
+    				<tr>
+      					<th style="text-align: center">Nom du parking</th>
+      					<th style="text-align: center">Total des places</th>
+      					<th style="text-align: center">Nombre de places libres</th>
+    				</tr>
+  				</thead>
+    			<tbody>	
+    			<?php
+    				$reponse2 = $bdd->query("SELECT * FROM parking WHERE zone_park = '$nomzone'");
+
+					while($donnees2 = $reponse2->fetch())
+					{	
+				?>
+    						<tr>
+        						<td><center><?php echo $donnees2['nom_park']; ?></center></td>
+        						<td><center><?php echo $donnees2['nbplaces_park']; ?> places </center></td>
+        						<td><center><?php echo $donnees2['free_places']; ?> places</center></td>
+      						</tr>
+				<?php
+					}
+					$reponse2->closeCursor(); // Termine le traitement de la requête
+				?>
+				</tbody>
+			</table>
+		</section>			    
+		<div class="container">
+
+  			
+  		<div class="col-md-offset-3 col-md-6">
+    			<button type="submit" class="btn btn-info" id="afficher">
+    				<span class="glyphicon glyphicon-pencil"></span>&nbsp;Modifier ce parking
+    			</button>
+    			<button type="submit" class="btn btn-info" id="afficher">
+    				<span class="glyphicon glyphicon-pencil"></span>&nbsp;Supprimer ce parking
+    			</button>
+
+    			<button type="submit" class="btn btn-info" id="afficher">
+    				<span class="glyphicon glyphicon-pencil"></span>&nbsp;Ajouter un parking
+    			</button>                                                                        
   			</div>
 
 		</div>
