5b6e067f39cd583596f11c30b883d10b8c4f18db
oumaimata <oumaima.ta@gmail.com>
2015-05-30 02:42:25 +0200

    transfert park quasi fini

--- adminCreation.php
+++ adminCreation.php
@@ -42,7 +42,7 @@
     <div class="container">
         <div id="alertChoixZone"class="alert alert-info alert-dismissable col-md-offset-2 col-md-8" style="display: none">
           <button type="button" class="close" id="closeChoixZone">×</button>
-        <form method="post" action="adminStats.php">
+        <form method="post" action="adminCreation.php">
           <h3 class="panel-title">Choisir une zone</h3>
             <select name="zone"class="selectpicker">
             <?php
@@ -69,10 +69,11 @@
     </div>
 
 
+
+
+<!--///////////////// Afficher les parkings de la zone selectionee-->
+
 <?php 
-
-///////////////// Afficher les parkings de la zone selectionee
-
       $nomzone = $_POST['zone'];
 //      $reponse2 = $bdd->query("SELECT nom_park FROM parking WHERE zone_park = '$nomzone'");
 //      $donnees2 = $reponse2->fetch();
@@ -109,24 +110,24 @@
       </table>          
 </section>
 
-<!--////////////// Suppression d un des parking de la zone selectionnee -->
+<!--////////////// Suppression d'un des parking de la zone selectionnee -->
 
     <div class="container">
         <div id="alertSupprPark" class="alert alert-info alert-dismissable col-md-offset-2 col-md-8" style="display: none">
           <button type="button" class="close" id="closeSupprPark">×</button>
-        <form method="post" action="test.php">
+        <form method="post" action="adminCreation.php">
           <h3 class="panel-title">Choisir un parking</h3>
             <select name="parkingsuppr"class="selectpicker">
             <?php
-              $reponse2= $bdd->query("SELECT * FROM parking WHERE zone_park = '$nomzone'");
-            while ($donnees2 = $reponse2->fetch())
+              $reponse3= $bdd->query("SELECT * FROM parking WHERE zone_park = '$nomzone'");
+            while ($donnees3 = $reponse3->fetch())
             {
           ?>
-                <option ><?php echo $donnees2['nom_park'];?></option>
+                <option ><?php echo $donnees3['nom_park'];?></option>
             <?php
             }
             
-            $reponse2->closeCursor();
+            $reponse3->closeCursor();
           ?>
             </select>
             
@@ -134,36 +135,61 @@
               <button type="submit">Supprimer</button>
         </form> 
         </div>
+
+<?php
+    $parking = $_POST['parkingsuppr'];
+    $reponse4 = $bdd->query("DELETE FROM parking WHERE nom_park = '$parking'");
+    $reponse4->closeCursor();
+    
+?>
 <!--/////////// Modification des infos d'un des parkings de la zone -->
         
         <div id="alertModifPark" class="alert alert-info alert-dismissable col-md-offset-2 col-md-8" style="display: none">
           <button type="button" class="close" id="closeModifPark">×</button>
-        <form method="post" action="test.php">
-          <h3 class="panel-title">Choisir un parking</h3>
+        <form method="post" action="adminCreation.php">
+          <h3 class="panel-title">Choisir le parking et la zone de transfert</h3>
             <select name="parkingmodif"class="selectpicker">
-            <?php
-              $reponse2= $bdd->query("SELECT * FROM parking WHERE zone_park = '$nomzone'");
-            while ($donnees2 = $reponse2->fetch())
+          <?php
+              $reponse5= $bdd->query("SELECT * FROM parking WHERE zone_park = '$nomzone'");
+            while ($donnees5 = $reponse5->fetch())
             {
           ?>
-                <option ><?php echo $donnees2['nom_park'];?></option>
-            <?php
+                <option ><?php echo $donnees5['nom_park'];?></option>
+          <?php
             }
             
-            $reponse2->closeCursor();
+           $reponse5->closeCursor();
           ?>
             </select>
-              <input name="prix" type="text" placeholder="nouveau prix">
- 
-              <button type="submit">Modifier</button>
+            <select name="zonetransfert"class="selectpicker">
+            <?php
+              $reponse6 = $bdd->query("SELECT nom_zone FROM zone");
+            while ($donnees6 = $reponse6->fetch())
+            {
+          ?>
+                <option><?php echo $donnees6['nom_zone'];?></option>
+          <?php
+            }
+            $reponse5->closeCursor();
+          ?>
+
+            </select>
+              <button type="submit">Transférer</button>
         </form> 
         </div>
 
-
+<?php
+    $parkingmodif = $_POST['parkingmodif'];
+    $zonetransfert = $_POST['zonetransfert'];
+    $reponse7 = $bdd->query(" UPDATE parking SET zone_park = '$zonetransfert' WHERE nom_park = '$parkingmodif'");
+    $reponse7->closeCursor();
+        
+?>
+      
 
         <div class="col-md-offset-3 col-md-2">
           <button type="submit" class="btn btn-info" id="modifPark">
-            <span class="glyphicon glyphicon-pencil"></span>&nbsp;Modifier un parking
+            <span class="glyphicon glyphicon-pencil"></span>&nbsp;Transférer un parking
           </button>
         </div>
 
