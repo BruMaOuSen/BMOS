e52616b37ca7922e781c654b9b1ad222e70959d3
MarieKromwel <mariekromwel@gmail.com>
2015-06-09 08:10:49 +0200

    avancement

--- adminStats.php
+++ adminStats.php
@@ -100,14 +100,12 @@
 					?>
     						<tr>
         						<td><?php echo $donnees['nom_park']; ?></td>
-							<?php $reponse2 = $bdd->query("SELECT SUM(prix) AS annuel FROM transac WHERE date_achat BETWEEN to_timestamp(to_char(Now()::timestamptz, 'YYYY')||'-01-01', 'YYYY-MM-DD') AND Now() AND nom_park = '$park'");
-						$reponse3 = $bdd->query("SELECT SUM(prix) AS mensuel FROM transac WHERE date_achat BETWEEN to_timestamp(to_char(Now()::timestamptz, 'YYYY-MM')||'-01', 'YYYY-MM-DD') AND Now() AND nom_park='$park'"); 
-							$donnees2 = $reponse2->fetch();
-							$donnees3 = $reponse3->fetch();
-							?>
-							
-        						<td><?php echo $donnees2['mensuel']; ?>€</td>
-        						<td><?php echo $donnees3['annuel']; ?>€</td>
+							<?php $reponse2 = $bdd->query("SELECT SUM(prix) AS sommen FROM transac WHERE date_achat BETWEEN to_timestamp(to_char(Now()::timestamptz, 'YYYY-MM')||'-01', 'YYYY-MM-DD') AND Now() AND nom_park='$park'"); 
+							$donnees2 = $reponse2->fetch(); ?>
+							<td><?php echo $donnees2['sommen']; ?>€</td>
+							<?php $reponse3 = $bdd->query("SELECT SUM(prix) AS somannu FROM transac WHERE date_achat BETWEEN to_timestamp(to_char(Now()::timestamptz, 'YYYY')||'-01-01', 'YYYY-MM-DD') AND Now() AND nom_park = '$park'");
+							$donnees3 = $reponse3->fetch();	?>
+							<td><?php echo $donnees3['somannu']; ?>€</td>
       						</tr>
 					<?php
 						}
@@ -116,135 +114,6 @@
         </tbody>
       </table>          
 </section>
-
-<!--////////////// Suppression d'un des parking de la zone selectionnee -->
-
-    <div class="container">
-        <div id="alertSupprPark" class="alert btn-danger alert-dismissable col-md-offset-2 col-md-8" style="display: none">
-          <button type="button" class="close" id="closeSupprPark">×</button>
-        <form method="post" action="adminCreation.php">
-          <h3 class="panel-title">Choisir un parking</h3>
-            <select name="parkingsuppr"class="selectpicker">
-            <?php
-              $reponse3= $bdd->query("SELECT * FROM parking WHERE zone_park = '$nomzone' ORDER BY nom_park");
-            while ($donnees3 = $reponse3->fetch())
-            {
-          ?>
-                <option ><?php echo $donnees3['nom_park'];?></option>
-            <?php
-            }
-            
-            $reponse3->closeCursor();
-          ?>
-            </select>
-            
- 
-              <button type="submit" class="btn btn-danger">Supprimer</button>
-        </form> 
-        </div>
-
-<?php
-    $parking = $_POST['parkingsuppr'];
-    $reponse4 = $bdd->query("DELETE FROM parking WHERE nom_park = '$parking'");
-    $reponse4->closeCursor();
-    
-?>
-<!--/////////// Modification des infos d'un des parkings de la zone -->
-        
-        <div id="alertModifPark" class="alert btn-info alert-dismissable col-md-offset-2 col-md-8" style="display: none">
-          <button type="button" class="close" id="closeModifPark">×</button>
-        <form method="post" action="adminCreation.php">
-          <h3 class="panel-title">Choisir le parking et la zone de transfert</h3>
-            <select name="parkingmodif"class="selectpicker">
-          <?php
-              $reponse5= $bdd->query("SELECT * FROM parking WHERE zone_park = '$nomzone' ORDER BY nom_park");
-            while ($donnees5 = $reponse5->fetch())
-            {
-          ?>
-                <option ><?php echo $donnees5['nom_park'];?></option>
-          <?php
-            }
-            
-           $reponse5->closeCursor();
-          ?>
-            </select>
-            <select name="zonetransfert"class="selectpicker">
-            <?php
-              $reponse6 = $bdd->query("SELECT nom_zone FROM zone EXCEPT (SELECT nom_zone FROM zone WHERE nom_zone = '$nomzone' )ORDER BY nom_zone");
-            while ($donnees6 = $reponse6->fetch())
-            {
-          ?>
-                <option><?php echo $donnees6['nom_zone'];?></option>
-          <?php
-            }
-            $reponse6->closeCursor();
-          ?>
-
-            </select>
-              <button type="submit" class="btn btn-info">Transférer</button>
-        </form> 
-        </div>
-
-<?php
-    $parkingmodif = $_POST['parkingmodif'];
-    $zonetransfert = $_POST['zonetransfert'];
-    $reponse7 = $bdd->query(" UPDATE parking SET zone_park = '$zonetransfert' WHERE nom_park = '$parkingmodif'");
-    $reponse7->closeCursor();
-        
-?>
-      
-<!--////////////////Ajouter un parking dasn la zone selectionnee -->
-
-<?php $_SESSION['nomZone'] = $nomzone;?>
-        <div id="alertAjoutPark" class="alert btn-success alert-dismissable col-md-offset-2 col-md-8" style="display: none">
-          <button type="button" class="close" id="closeAjoutPark">×</button>
-        <form method="post" action="test2.php">
-          <h3 class="panel-title">Ajouter un parking dans la zone <?php echo $nomzone;?></h3>
-            <div class="form-group">
-                <input id="nomParking" name="nomParking" type="text" placeholder="Nom du parking" class="form-control">
-            </div>
-          <div class="form-group">
-                <input id="RC2"  name="RC2" type="text" placeholder="Nombres de places COUVERTES pour les 2 roues" class="form-control">
-            </div>
-            <div class="form-group">
-                <input id="RD2" name="RD2" type="text" placeholder="Nombres de places DEHORS pour les 2 roues" class="form-control">
-            </div>
-            <div class="form-group">
-                <input id="RC4"  name="RC4" type="text" placeholder="Nombres de places COUVERTES pour les 4 roues" class="form-control">
-            </div>
-            <div class="form-group">
-                <input id="RD4" name="RD4" type="text" placeholder="Nombres de places DEHORS pour les 4 roues" class="form-control">
-            </div><div class="form-group">
-                <input id="RC8"  name="RC8" type="text" placeholder="Nombres de places COUVERTES pour les 8 roues" class="form-control">
-            </div>
-            <div class="form-group">
-                <input id="8RD" name="RD8" type="text" placeholder="Nombres de places DEHORS pour les 8 roues" class="form-control">
-            </div>
-              <button type="submit" class="btn btn-primary">Ajouter</button>
-        </form> 
-        </div>
-      
-       	<div class="col-md-offset-2 col-md-8"> 
-       		<div class="btn-group btn-group-justified" role="group"> 
-        		<div class="btn-group"  role="group">
-        			<button type="submit" class="btn btn-success" id="ajoutPark">
-        		    	<span class="glyphicon glyphicon-ok"></span>&nbsp;Ajouter un parking
-          		  	</button>                                                                        
-     	 		</div>
-     	 		<div class="btn-group"  role="group">
-         			<button type="button" class="btn btn-info" id="modifPark">
-            			<span class="glyphicon glyphicon-pencil"></span>&nbsp;Transférer un parking
-          			</button>
-        		</div>
-     	 		<div class="btn-group"  role="group">
-          			<button type="submit" class="btn btn-danger" id="supprPark">
-            			<span class="glyphicon glyphicon-remove"></span>&nbsp;Supprimer un parking
-          			</button>
-        		</div>
-    		</div>
-      </div>
-
-
 
 
 <script src="bootstrap/js/jquery.js"></script> 
